dec25887d5fd7334f122c3e23d24bd7e75016bbb
Dennis Sammy <dennissammy77@gmail.com>
2025-04-07 10:58:29 +0300

    chore: savedJobs user model update

--- models/user.model.js
+++ models/user.model.js
@@ -12,13 +12,20 @@ const userSchema = new mongoose.Schema({
   preferences: {
     jobTitle: String, // e.g. Full stack developer
     jobType: String, // e.g. ['Full-time', 'Remote']
-    industries: String, // e.g. ['Tech', 'Finance']
     location: { id: String, name: String}, // e.g. ['New York', 'Kenya']
     experienceLevel: String, // e.g. "Mid", "Senior"
     experienceYears: String // e.g. "0-1", "1-3", "3-5", "5+"
   },
-  savesJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'SavedJob' }],
-  appliedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'AppliedJob' }],
+  savedJobs: [
+    {
+      jobId: String,
+      savedAt: Date,
+      coverLetter: String, // cover letter created by AI
+      resumeUrl: String, // resume created by AI
+      atsScore: Number, // score created by AI
+      status: String // e.g. "Applied", "Interviewed", "Offered", "Declined"
+    }
+  ],
   resume: { title: String, fileUrl: String }
 },{timestamps: true});
 
